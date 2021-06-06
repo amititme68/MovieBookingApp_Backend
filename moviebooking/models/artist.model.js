a227f14877fa7c9481a918e963148ef0d52afc36
amititme68 <amititme68@gmail.com>
2021-06-06 22:21:31 +0530

    Checkpoint8

--- moviebooking/models/artist.model.js
+++ moviebooking/models/artist.model.js
@@ -1,23 +1,15 @@
-module.exports = mongoose => {
+module.exports = (mongoose) => {
   const Artist = mongoose.model(
-      "artist",
-      mongoose.Schema(
-        {
-          artistid:Number,
-          first_name:String,
-          last_name:String,
-          wiki_url:String,
-          profile_url:String,
-          movies: [
-            {
-              type: String,
-            },
-          ],
-        },
-          
-      )
-    );
-  
-    return Artist;
-  };
+    "artist",
+    mongoose.Schema({
+      artistid: Number,
+      first_name: String,
+      last_name: String,
+      wiki_url: String,
+      profile_url: String,
+      movies: [String],
+    })
+  );
 
+  return Artist;
+};
--- moviebooking/models/genre.model.js
+++ moviebooking/models/genre.model.js
@@ -1,14 +1,11 @@
-module.exports = mongoose => {
-    const Genre = mongoose.model(
-        "genres",
-        mongoose.Schema(
-          {
-            genreid:Number,
-            genre:String,
-          },
-            
-        )
-      );
-    
-      return Genre;
-    };
+module.exports = (mongoose) => {
+  const Genre = mongoose.model(
+    "genres",
+    mongoose.Schema({
+      genreid: Number,
+      genre: String,
+    })
+  );
+
+  return Genre;
+};
